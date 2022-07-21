81c57f4f56e8223b7bf624afc4e5d13755c92401
tomoron <tomoron326@outlook.jp>
2022-07-21 18:38:26 +0900

    add:name requeset

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -5,10 +5,13 @@ class QuizzesController < ApplicationController
   def index
     uri = URI.parse('https://pokeapi.co')
     get_request = Net::HTTP::Get.new('/api/v2/pokemon/1', 'Content-Type' => 'application/json')
+    name_request = Net::HTTP::Get.new('/api/v2/pokemon-species/1', 'Content-Type' => 'application/json')
     http_client= Net::HTTP.new(uri.host,uri.port)
     http_client.use_ssl = true
     response = http_client.request(get_request)
+    name_response = http_client.request(name_request)
     @pokemon = JSON.parse(response.body)
+    @pokemon_name = JSON.parse(name_response.body)
 
 
   end
