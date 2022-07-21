2ec3ba6f6b7fbab50d7f1520d0bb32489aa15671
tomoron <tomoron326@outlook.jp>
2022-07-21 16:10:30 +0900

    add:http

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,15 @@
+require 'net/http'
+
 class QuizzesController < ApplicationController
 
   def index
+    uri = URI.parse('https://pokeapi.co')
+    get_request = Net::HTTP::Get.new('/api/v2/pokemon/1', 'Content-Type' => 'application/json')
+    http_client= Net::HTTP.new(uri.host,uri.port)
+    http_client.use_ssl = true
+    response = http_client.request(get_request)
+    @pokemon = JSON.parse(response.body)
+
 
   end
 
