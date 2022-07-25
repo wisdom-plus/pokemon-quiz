2e9d294f458c907b8f62bd8a7d68dfdddb470240
tomoron <tomoron326@outlook.jp>
2022-07-25 21:33:06 +0900

    add:rand

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -3,9 +3,10 @@ require 'net/http'
 class QuizzesController < ApplicationController
 
   def index
+    poke_id = rand(1...905)
     uri = URI.parse('https://pokeapi.co')
-    get_request = Net::HTTP::Get.new('/api/v2/pokemon/1', 'Content-Type' => 'application/json')
-    name_request = Net::HTTP::Get.new('/api/v2/pokemon-species/1', 'Content-Type' => 'application/json')
+    get_request = Net::HTTP::Get.new("/api/v2/pokemon/#{poke_id}", 'Content-Type' => 'application/json')
+    name_request = Net::HTTP::Get.new("/api/v2/pokemon-species/#{poke_id}", 'Content-Type' => 'application/json')
     http_client= Net::HTTP.new(uri.host,uri.port)
     http_client.use_ssl = true
     response = http_client.request(get_request)
