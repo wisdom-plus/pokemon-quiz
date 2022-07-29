e69524d1efc9d51c56938ea0cd39c578778e7150
tomoron <tomoron326@outlook.jp>
2022-07-29 20:35:52 +0900

    fix:typo

--- app/models/poke.rb
+++ app/models/poke.rb
@@ -7,15 +7,15 @@ class Poke
   def get_pokemon_data
     http_client = client
     get_request = Net::HTTP::Get.new("/api/v2/pokemon/#{@poke_id}", 'Content-Type' => 'application/json')
-    response = http_client.request(get_request)@pokemon_name = JSON.parse(name_response.body)
-    @pokemon = JSON.parse(response.body)
+    response = http_client.request(get_request)
+    pokemon = JSON.parse(response.body)
   end
 
   def get_pokemon_jp_name
     http_client = client
     name_request = Net::HTTP::Get.new("/api/v2/pokemon-species/#{@poke_id}", 'Content-Type' => 'application/json')
     name_response = http_client.request(name_request)
-    @pokemon_name = JSON.parse(name_response.body)
+    pokemon_name = JSON.parse(name_response.body)
   end
 
   private
@@ -23,6 +23,6 @@ class Poke
   def client(uri = @uri)
     http_client = Net::HTTP.new(uri.host,uri.port)
     http_client.use_ssl = true
-    hattp_client
+    http_client
   end
 end
