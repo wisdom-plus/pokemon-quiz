8cfa44d68b84929700d624ef9d7602aebcf46a38
tomoron <tomoron326@outlook.jp>
2022-07-29 20:34:12 +0900

    replace:poke

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,17 +2,9 @@ class QuizzesController < ApplicationController
 
   def index
     poke_id = rand(1...905)
-    uri = URI.parse('https://pokeapi.co')
-    get_request = Net::HTTP::Get.new("/api/v2/pokemon/#{poke_id}", 'Content-Type' => 'application/json')
-    name_request = Net::HTTP::Get.new("/api/v2/pokemon-species/#{poke_id}", 'Content-Type' => 'application/json')
-    http_client= Net::HTTP.new(uri.host,uri.port)
-    http_client.use_ssl = true
-    response = http_client.request(get_request)
-    name_response = http_client.request(name_request)
-    @pokemon = JSON.parse(response.body)
-    @pokemon_name = JSON.parse(name_response.body)
-
-
+    poke = Poke.new(poke_id)
+    @pokemon = poke.get_pokemon_data
+    @pokemon_name = poke.get_pokemon_jp_name
   end
 
   def result
