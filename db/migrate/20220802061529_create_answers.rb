5246db91a2d4b3399e3f0a1a7633f6ea0d4c84ea
tomoron <tomoron326@outlook.jp>
2022-08-02 15:41:04 +0900

    add:db answer

---
+++ db/migrate/20220802061529_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :answers do |t|
+      t.string :content
+      t.bigint :poke_id
+      t.bigint :user_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_061529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "content"
+    t.bigint "poke_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
