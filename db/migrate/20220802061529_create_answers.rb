721d6e1d4a548b64c7dece65dbce4ccaa3194eba
tomoron <tomoron326@outlook.jp>
2022-08-24 12:05:19 +0900

    add:column correct

--- db/migrate/20220802061529_create_answers.rb
+++ db/migrate/20220802061529_create_answers.rb
@@ -3,6 +3,7 @@ class CreateAnswers < ActiveRecord::Migration[7.0]
     create_table :answers do |t|
       t.string :content
       t.bigint :poke_id
+      t.boolean :correct
       t.timestamps
     end
   end
