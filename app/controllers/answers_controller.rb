237239a090aef6490d45d6986a04ae8baa0d5d49
tomoron <tomoron326@outlook.jp>
2022-08-24 11:30:03 +0900

    add:create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,11 @@
 class AnswersController < ApplicationController
   def create
+    @answer = Answer.new(answer_params)
+    @answer.user_id = current_user.id
+    if @answer.save
+      redirect_to quizzes_result_path
+    else
+      redirect_to root_path
+    end
   end
 end
