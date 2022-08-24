d063703c7905291adfeb20ea0d6cabbb8a3e98f3
tomoron <tomoron326@outlook.jp>
2022-08-24 12:01:46 +0900

    add:answers controller create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,16 @@
 class AnswersController < ApplicationController
   def create
     @answer = Answer.new(answer_params)
-    @answer.user_id = current_user.id
     if @answer.save
       redirect_to quizzes_result_path
     else
       redirect_to root_path
     end
+  end
+
+
+  private
+  def answer_params
+    params.require(:answer).permit(:content,:poke_id)
   end
 end
