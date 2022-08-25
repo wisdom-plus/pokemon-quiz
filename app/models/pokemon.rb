15e13bf8e236eef43ac822814ae0b484dc3b5bdc
tomoron <tomoron326@outlook.jp>
2022-08-25 18:23:37 +0900

    add:model pokemon

---
+++ app/models/pokemon.rb
@@ -0,0 +1,25 @@
+class Pokemon
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+  include ActiveModel::Serializers::JSON
+
+  attr_accessor :id, :image, :name
+  attribute :id, :string
+  attribute :image, :string
+  attribute :name, :string
+
+  def initialize(id)
+    @id = id
+  end
+
+  def fetch
+    poke = Poke.new(@id)
+    if poke.get_pokemon_data
+      @image = poke.data[:sprites][:other][:'official-artwork'][:front_default]
+      @name = poke.get_pokemon_jp_name
+    else
+      false
+    end
+  end
+
+end
