8251116fb566c4af84641ce217329f5fbe08ec95
tomoron <tomoron326@outlook.jp>
2022-08-25 18:29:40 +0900

    pref:result

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -8,13 +8,8 @@ class QuizzesController < ApplicationController
 
   def result
     @answer = Answer.last
-    poke = Poke.new(@answer.poke_id)
-    begin
-      @pokemon = poke.get_pokemon_data
-      @pokemon_name = poke.get_pokemon_jp_name
-    rescue => e
-      redirect_to root_path
-    end
+    @pokemon = Pokemon.new(@answer.poke_id)
+    redirect_to root_path unless @pokemon.fetch
   end
 
   private
