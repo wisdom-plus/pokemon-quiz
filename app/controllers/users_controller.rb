115458c0d22c573d39e8792d6d1302ace7a289b8
tomoron <tomoron326@outlook.jp>
2022-08-28 22:59:54 +0900

    add:cookies permanent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      cookie[:token] = user.token
+      cookies.permanent[:token] = user.token
       redirect_to quizzes_path
     else
       render :new
