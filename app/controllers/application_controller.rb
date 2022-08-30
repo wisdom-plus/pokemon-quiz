2ade82a1dfc8f1679b10cf0b5c68aa424a016af3
tomoron <tomoron326@outlook.jp>
2022-08-30 20:11:48 +0900

    add:current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  private
+
+    def current_user
+      return nil unless cookies[:token]
+
+      User.find_by(token: cookies[:token])
+    end
 end
