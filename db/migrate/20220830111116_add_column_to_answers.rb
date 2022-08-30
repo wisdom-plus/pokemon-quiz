1a578297a5e912e6e6cdf3cbaea567e8961b0e2f
tomoron <tomoron326@outlook.jp>
2022-08-30 20:14:30 +0900

    add:colmun to answers user_id

---
+++ db/migrate/20220830111116_add_column_to_answers.rb
@@ -0,0 +1,5 @@
+class AddColumnToAnswers < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :answers, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_113920) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_111116) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_113920) do
     t.boolean "correct", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +31,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_113920) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "answers", "users"
 end
