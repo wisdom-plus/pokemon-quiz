f2876ed1e8dd1d773122e3b1b64a1d2e79c1df36
tomoron <tomoron326@outlook.jp>
2022-09-05 16:19:25 +0900

    add:translate methos

--- app/javascript/controllers/translation_controller.js
+++ app/javascript/controllers/translation_controller.js
@@ -1,7 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="translation"
 export default class extends Controller {
-  connect() {
+  static targets = ["text"];
+  static values = { input_text: String };
+
+  translate() {
+    const text = this.textTarget.value;
+    const translate_text = text.replace(/[ぁ-ん]/g, function (s) {
+      return String.fromCharCode(s.charCodeAt(0) + 0x60);
+    });
+    this.textTarget.value = translate_text;
   }
 }
