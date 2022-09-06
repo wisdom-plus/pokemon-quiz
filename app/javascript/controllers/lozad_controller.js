32f6ff49ce61b93f93b0029fd3f5129814c1344e
tomoron <tomoron326@outlook.jp>
2022-09-06 19:20:35 +0900

    add:remove class

--- app/javascript/controllers/lozad_controller.js
+++ app/javascript/controllers/lozad_controller.js
@@ -6,9 +6,11 @@ export default class extends Controller {
   static targets = ["spinner"];
   connect() {
     const loading = this.spinnerTarget;
+    const container = document.getElementById("image-container");
     const observer = lozad(".lozad", {
       loaded: function (el) {
         loading.classList.add("hidden");
+        container.classList.remove("w-1/2");
       },
     });
 
